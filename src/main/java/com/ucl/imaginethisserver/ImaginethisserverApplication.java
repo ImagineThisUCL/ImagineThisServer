7f3cd2231d639b6456e2a33fd70be2b7927a98f9
Yutong Wang <598402463@qq.com>
2020-07-14 16:49:31 +0800

    initialize an spring boot project

---
+++ src/main/java/com/ucl/imaginethisserver/ImaginethisserverApplication.java
@@ -0,0 +1,13 @@
+package com.ucl.imaginethisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaginethisserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImaginethisserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ucl/imaginethisserver/ImaginethisserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucl.imaginethisserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaginethisserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
