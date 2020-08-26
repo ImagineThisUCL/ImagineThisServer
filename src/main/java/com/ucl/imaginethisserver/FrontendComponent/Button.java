659963da0004800813ba494494773d3c3ce7ee56
Vojta Adam <bluetrox@hotmail.com>
2020-08-27 00:39:21 +0200

    Changing width to maxWidth to render buttons more accurately

--- src/main/java/com/ucl/imaginethisserver/FrontendComponent/Button.java
+++ src/main/java/com/ucl/imaginethisserver/FrontendComponent/Button.java
@@ -61,7 +61,7 @@ public class Button extends FrontendComponent{
                 String navigateWireframe = Page.getWireframeByID(transitionNodeID).getName().replaceAll(" ", "");
                 buttonCode.append("onPress={() => this.props.navigation.navigate('").append(navigateWireframe).append("')}\n");
             }
-            buttonCode.append("   style={{backgroundColor:").append(backgroundColor).append(", marginTop: base.margin");
+            buttonCode.append("   style={{backgroundColor:").append(backgroundColor).append(", marginTop: base.margin, minWidth: " + this.width);
             if (!isCircle) {
                 buttonCode.append(", borderRadius: ").append(this.cornerRadius);
             }
