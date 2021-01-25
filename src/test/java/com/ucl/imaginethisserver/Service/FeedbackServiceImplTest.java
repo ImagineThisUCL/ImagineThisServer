56331efab49505a06267c4499f5ef042c8fbb25d
Lukas Cerny <cerny.lukas.oto@gmail.com>
2021-01-25 15:31:30 +0100

    First draft of unit tests.

---
+++ src/test/java/com/ucl/imaginethisserver/Service/FeedbackServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.ucl.imaginethisserver.Service;
+
+import com.ucl.imaginethisserver.Model.Feedback;
+import com.ucl.imaginethisserver.Model.Vote;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FeedbackServiceImplTest {
+
+    @BeforeAll
+    static void mockDAO() {
+        // TODO: Mock class and methods implementing FeedbackDAO interface
+
+    }
+
+    @Test
+    void getAllFeedbacksTest() {
+        // TODO: Check method returns correct mocked data
+
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void getFeedbackByIDTest() {
+        // TODO: Check method returns correct mocked data
+
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void addNewFeedbackTest() {
+        // TODO: Check method addNewFeedback is called on DAO class
+
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void voteFeedbackTest() {
+        // TODO: Check method voteFeedback is called on DAO class
+
+        assertEquals(1, 1);
+
+    }
+
+
+}
