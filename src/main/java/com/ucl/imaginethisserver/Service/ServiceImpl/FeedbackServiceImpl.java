60af0bff5e9de6bbb7606c1530eceb020c40c752
Robin <bbaayezi@gmail.com>
2021-01-26 01:40:42 +0000

    implement feedback service with mock data

--- src/main/java/com/ucl/imaginethisserver/Service/ServiceImpl/FeedbackServiceImpl.java
+++ src/main/java/com/ucl/imaginethisserver/Service/ServiceImpl/FeedbackServiceImpl.java
@@ -3,19 +3,30 @@ package com.ucl.imaginethisserver.Service.ServiceImpl;
 import com.ucl.imaginethisserver.Model.Feedback;
 import com.ucl.imaginethisserver.Model.Vote;
 import com.ucl.imaginethisserver.Service.FeedbackService;
+import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class FeedbackServiceImpl implements FeedbackService {
     @Override
     public List<Feedback> getAllFeedbacks(String projectID) {
-        return null;
+        // mock data
+        List<Feedback> feedbacks = new ArrayList<>();
+        feedbacks.add(new Feedback(UUID.randomUUID(), "MgWqYTZMdjG26oA1CxbWaE", UUID.randomUUID(),
+                "John", new Timestamp(System.currentTimeMillis()), "Good prototype!"));
+        feedbacks.add(new Feedback(UUID.randomUUID(), "MgWqYTZMdjG26oA1CxbWaE", UUID.randomUUID(),
+                "Elena", new Timestamp(System.currentTimeMillis()), "I don't like the background color of the theme. Too dark."));
+        return feedbacks;
     }
 
     @Override
     public Feedback getFeedbackByID(String projectID, UUID feedbackID) {
-        return null;
+        return new Feedback(UUID.randomUUID(), projectID, feedbackID,
+                "John", new Timestamp(System.currentTimeMillis()), "Good prototype!");
     }
 
     @Override
