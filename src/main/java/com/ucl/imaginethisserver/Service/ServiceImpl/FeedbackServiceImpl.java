61cd92b7681e12ef9e54af57cc2d00c71752575c
Robin <bbaayezi@gmail.com>
2021-01-26 11:52:44 +0000

    move data mocking process to DAO implementation

--- src/main/java/com/ucl/imaginethisserver/Service/ServiceImpl/FeedbackServiceImpl.java
+++ src/main/java/com/ucl/imaginethisserver/Service/ServiceImpl/FeedbackServiceImpl.java
@@ -1,41 +1,42 @@
 package com.ucl.imaginethisserver.Service.ServiceImpl;
 
+import com.ucl.imaginethisserver.DAO.FeedbackDAO;
 import com.ucl.imaginethisserver.Model.Feedback;
 import com.ucl.imaginethisserver.Model.Vote;
 import com.ucl.imaginethisserver.Service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class FeedbackServiceImpl implements FeedbackService {
+
+    private final FeedbackDAO feedbackDAO;
+
+    @Autowired
+    public FeedbackServiceImpl(FeedbackDAO feedbackDAO) {
+        this.feedbackDAO = feedbackDAO;
+    }
+
     @Override
     public List<Feedback> getAllFeedbacks(String projectID) {
-        // mock data
-        List<Feedback> feedbacks = new ArrayList<>();
-        feedbacks.add(new Feedback(UUID.randomUUID(), "MgWqYTZMdjG26oA1CxbWaE", UUID.randomUUID(),
-                "John", new Timestamp(System.currentTimeMillis()), "Good prototype!"));
-        feedbacks.add(new Feedback(UUID.randomUUID(), "MgWqYTZMdjG26oA1CxbWaE", UUID.randomUUID(),
-                "Elena", new Timestamp(System.currentTimeMillis()), "I don't like the background color of the theme. Too dark."));
-        return feedbacks;
+        return feedbackDAO.getAllFeedbacks(projectID);
     }
 
     @Override
     public Feedback getFeedbackByID(String projectID, UUID feedbackID) {
-        return new Feedback(UUID.randomUUID(), projectID, feedbackID,
-                "John", new Timestamp(System.currentTimeMillis()), "Good prototype!");
+        return feedbackDAO.getFeedbackByID(projectID, feedbackID);
     }
 
     @Override
     public boolean addNewFeedback(String projectID, Feedback feedback) {
-        return false;
+        return feedbackDAO.addNewFeedback(projectID, feedback);
     }
 
     @Override
     public boolean voteFeedback(String projectID, UUID feedbackID, Vote vote) {
-        return false;
+        return feedbackDAO.voteFeedback(projectID, feedbackID, vote);
     }
 }
