90422b5be83b2db3d9b235b192ee3f791cda6527
Robin <bbaayezi@gmail.com>
2021-02-01 01:36:15 +0000

    Setup tests for FeedbackController

---
+++ src/test/java/com/ucl/imaginethisserver/Service/ServiceImpl/FeedbackServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.ucl.imaginethisserver.Service.ServiceImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeedbackServiceImplTest {
+
+    @Test
+    void getAllFeedbacks() {
+    }
+
+    @Test
+    void getFeedbackByID() {
+    }
+
+    @Test
+    void addNewFeedback() {
+    }
+
+    @Test
+    void voteFeedback() {
+    }
+}
