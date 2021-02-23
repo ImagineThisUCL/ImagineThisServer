0dd0098cc98337797552d0e1caa6691255f01653
Robin <bbaayezi@gmail.com>
2021-02-23 22:40:26 +0000

    add options to disable swagger in production environment

--- src/main/java/com/ucl/imaginethisserver/Conf/SwaggerConfig.java
+++ src/main/java/com/ucl/imaginethisserver/Conf/SwaggerConfig.java
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile({"!prod"})
+@Profile({"!prod && swagger"})
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
