f9a1e294fe0d2b2c279aba1b2684f3d9eda4b1f0
Robin <bbaayezi@gmail.com>
2021-02-23 22:38:14 +0000

    add options to disable swagger in production environment

--- src/main/java/com/ucl/imaginethisserver/ImaginethisserverApplication.java
+++ src/main/java/com/ucl/imaginethisserver/ImaginethisserverApplication.java
@@ -1,12 +1,9 @@
 package com.ucl.imaginethisserver;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
-@EnableSwagger2
 public class ImaginethisserverApplication {
 
     public static void main(String[] args) {
